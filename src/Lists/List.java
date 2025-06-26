2b515bfa466f0eb7521fb817c2ed77e1d2d37954
SoapGiutroll <Giutroll117troll@gmail.com>
2025-06-26 13:31:17 -0600

    Se crea paquete Lists y interfas List
    se crea el paquete Lists y la inferaz List con la funcion de find y remove

---
+++ src/Lists/List.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author UTN
+ */
+public interface List <T>{
+    
+    
+    public T find(Object id);
+    
+    
+    public boolean remove(T t);
+    
+}
